83ee97331afa637d928f64517b610b2d2b06ce85
tanishqatp <164970434+tanishqatp@users.noreply.github.com>
2024-07-02 17:22:09 +0530

    Update intent_identification.py

--- intent_identification.py
+++ intent_identification.py
@@ -26,7 +26,7 @@ def identify_intent(user_text, img_url):
             messages=[
                 { "role": "system", "content": "You are an expert in fashion design. Based on the users intent,\
                 generate a prompt that would be useful as a search query in a vector database. Unless otherwise specified, provide details \
-                of color, style and material."},
+                of color, style and material. Provide these details in 5 or more bullet points"},
 
                 { "role": "user", "content": [  
                     { 
@@ -39,7 +39,17 @@ def identify_intent(user_text, img_url):
                             "url": img_url
                         }
                     }
-                ] } 
+                ] },
+                {
+                    "role": "assistant",
+                    "content":
+                        "Red women's shirt with ruffled and asymmetric hem, tie-front design, sleeveless, flowy fabric\
+                         - Category: Women's Shirt \
+                         - Style: Halter neck \
+                         - Style: Sleeveless \
+                         - Color: Bright Red \
+                         - Features: Flowy, tie-knot, ruffles "
+                }
             ],
             max_tokens=2000 
         )
@@ -51,8 +61,7 @@ def identify_intent(user_text, img_url):
                     The prompt may be a follow up to previous search result or a completely new question. Any questions about the material or style can be considered\
                     a follow up question. For example 'What is the material of the first result?' Any question asking for a new item to be searched \
                     can be considered a new question. For example 'Find me a dress for summer in yellow color.' \
-                    If it is a new question, generate a prompt that would be useful in a vector search, providing all relevant details such as color, style, fabric from the prompt. \
-                    Provide these details in bullet points. \
+                    If it is a new question, generate a prompt that would be useful in a vector search. \
                     If it is a follow up question, return the following statement exactly: \"This is a follow up\""},
 
                 { "role": "user", "content": [  
@@ -60,7 +69,7 @@ def identify_intent(user_text, img_url):
                         "type": "text", 
                         "text": user_text
                     },
-                ] } 
+                ] }, 
             ],
             max_tokens=2000 
         )
