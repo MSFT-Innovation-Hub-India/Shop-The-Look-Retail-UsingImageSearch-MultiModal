9a9c5f6e2fa49eba6489385251ce0b1e34881a44
tanishqatp <164970434+tanishqatp@users.noreply.github.com>
2024-07-09 16:06:34 +0530

    updated search frontend

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -10,16 +10,16 @@ export const Searchbar = () => {
             <input
               type="search"
               placeholder='How can I help'
-              className='fixed bottom-10 w-[80%] p-4 rounded-full bg-slate-800'
+              className='fixed bottom-11 w-[80%] p-4 rounded-3xl bg-slate-800'
             />
-            <button>
-              <FaMicrophone />
+            <button className='fixed bottom-11 p-4 bg-transparent right-[120px]'>
+              <FaMicrophone  color='#1E293B' size={25} />
             </button>
-            <button className='fixed bottom-10 mr- p-4'>
-              <IoSend />
+            <button className='fixed bottom-10 p-4 bg-transparent right-[182px]'>
+              <IoSend  color='white' size={30} />
             </button>
-            <button>
-              <MdOutlineFileUpload />
+            <button className='fixed bottom-10 p-4 bg-transparent right-[80px]'>
+              <MdOutlineFileUpload color='#1E293B' size={35} />
             </button>
           </div>
       </form>
