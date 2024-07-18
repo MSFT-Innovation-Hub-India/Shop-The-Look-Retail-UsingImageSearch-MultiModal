ec16d4f1a5b59f24d8956d55ec0704328c945aad
tanishqatp <164970434+tanishqatp@users.noreply.github.com>
2024-07-18 19:28:11 +0530

    Update chatbot.tsx

--- app/components/chatbot.tsx
+++ app/components/chatbot.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 import React, { useState } from 'react';
-import { FaMicrophone } from 'react-icons/fa';
-import { IoSend } from 'react-icons/io5';
-import { MdAddPhotoAlternate } from 'react-icons/md';
+import { IoMicOutline, IoImagesOutline, IoSend } from 'react-icons/io5';
 import axios from 'axios';
 import { useHeader } from './Header';
 
@@ -24,31 +22,31 @@ const Chatbot = () => {
 
   return (
     <div className="flex items-center justify-center h-screen bg-white">
-      <div className="justify-center pt-1 max-w-7xl w-full max-h-[70vh] h-auto flex-grow bg-white p-4 rounded-lg shadow-md overflow-y-auto">
+      <div className="justify-center pt-1 max-w-7xl w-full max-h-[70vh] h-auto flex-grow bg-white p-4 rounded-lg border-2 border-zinc-200 overflow-y-auto">
         {messages.map((msg, index) => (
           <p key={index} className={`mb-2 ${msg.startsWith('You') ? 'text-black' : 'text-black'}`}>
             {msg}
           </p>
         ))}
       </div>
-      <form onSubmit={sendMessage} className='fixed bottom-0 w-full mx-auto'>
+      <form onSubmit={sendMessage} className='fixed bottom-4 mx-auto bg-black w-[70%] p-0  rounded-3xl'>
         <div className="flex items-center justify-center py-2 space-x-2">
+        <button type="button" className='p-2 bg-transparent'>
+              <IoMicOutline color='white' size={30} />
+            </button>
+            <button type="button" className='p-2 bg-transparent'>
+              <IoImagesOutline color='white' size={30} />
+            </button>
           <input
             type="search"
             placeholder='How can I help'
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className='w-[70%] p-4 rounded-3xl bg-slate-800 text-white focus:outline-none'
+            className='w-[85%] p-4 rounded-3xl bg-black text-white focus:outline-none'
           />
           <div className="flex space-x-2"> 
-            <button type="button" className='p-2 bg-transparent'>
-              <FaMicrophone color='#1E293B' size={30} />
-            </button>
             <button type="submit" className='p-2 bg-transparent'>
-              <IoSend color='#1E293B' size={30} />
-            </button>
-            <button type="button" className='p-2 bg-transparent'>
-              <MdAddPhotoAlternate color='#1E293B' size={37} />
+              <IoSend color='white' size={30} />
             </button>
           </div>
         </div>
